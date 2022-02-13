32630a3645d77af6326751218369c995747632d8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-02-13 15:07:14 +0000

    Codion upgrade

--- world-client/src/main/java/is/codion/framework/demos/world/model/CountryModel.java
+++ world-client/src/main/java/is/codion/framework/demos/world/model/CountryModel.java
@@ -3,6 +3,8 @@ package is.codion.framework.demos.world.model;
 import is.codion.common.value.Value;
 import is.codion.framework.db.EntityConnectionProvider;
 import is.codion.framework.demos.world.domain.api.World.City;
+import is.codion.framework.demos.world.domain.api.World.Country;
+import is.codion.framework.demos.world.domain.api.World.CountryLanguage;
 import is.codion.framework.domain.entity.Entity;
 import is.codion.swing.framework.model.SwingEntityModel;
 import is.codion.swing.framework.model.SwingEntityTableModel;
@@ -13,8 +15,13 @@ public final class CountryModel extends SwingEntityModel {
 
   CountryModel(EntityConnectionProvider connectionProvider) {
     super(new CountryTableModel(connectionProvider));
+    getEditModel().initializeComboBoxModels(Country.CAPITAL_FK);
+
     SwingEntityModel cityModel = new SwingEntityModel(new CityTableModel(connectionProvider));
+    cityModel.getEditModel().initializeComboBoxModels(City.COUNTRY_FK);
     SwingEntityModel countryLanguageModel = new SwingEntityModel(new CountryLanguageTableModel(connectionProvider));
+    countryLanguageModel.getEditModel().initializeComboBoxModels(CountryLanguage.COUNTRY_FK);
+
     addDetailModels(cityModel, countryLanguageModel);
 
     cityModel.getTableModel().addRefreshListener(() ->
