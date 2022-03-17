e89015723167bd7961db8f076037aab3c8a43091
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2022-03-17 18:08:48 +0000

    Codion upgrade

--- world-domain/src/main/java/is/codion/framework/demos/world/domain/WorldImpl.java
+++ world-domain/src/main/java/is/codion/framework/demos/world/domain/WorldImpl.java
@@ -87,7 +87,7 @@ public final class WorldImpl extends DefaultDomain implements World {
             columnProperty(Country.INDEPYEAR_SEARCHABLE)
                     .columnExpression("to_char(indepyear)")
                     .searchProperty()
-                    .readOnly(),
+                    .readOnly(true),
             columnProperty(Country.POPULATION, "Population")
                     .nullable(false)
                     .numberFormatGrouping(true),
@@ -138,7 +138,7 @@ public final class WorldImpl extends DefaultDomain implements World {
                     .updatable(true),
             foreignKeyProperty(CountryLanguage.COUNTRY_FK, "Country"),
             columnProperty(CountryLanguage.IS_OFFICIAL, "Is official")
-                    .columnHasDefaultValue()
+                    .columnHasDefaultValue(true)
                     .nullable(false),
             derivedProperty(CountryLanguage.NO_OF_SPEAKERS, "No. of speakers",
                     new NoOfSpeakersProvider(), CountryLanguage.COUNTRY_FK, CountryLanguage.PERCENTAGE)
@@ -190,31 +190,31 @@ public final class WorldImpl extends DefaultDomain implements World {
   void continent() {
     define(Continent.TYPE, "world.country",
             columnProperty(Continent.NAME, "Continent")
-                    .groupingColumn()
+                    .groupingColumn(true)
                     .beanProperty("name"),
             columnProperty(Continent.SURFACE_AREA, "Surface area")
                     .columnExpression("sum(surfacearea)")
-                    .aggregateColumn()
+                    .aggregateColumn(true)
                     .numberFormatGrouping(true),
             columnProperty(Continent.POPULATION, "Population")
                     .columnExpression("sum(population)")
-                    .aggregateColumn()
+                    .aggregateColumn(true)
                     .numberFormatGrouping(true),
             columnProperty(Continent.MIN_LIFE_EXPECTANCY, "Min. life expectancy")
                     .columnExpression("min(lifeexpectancy)")
-                    .aggregateColumn(),
+                    .aggregateColumn(true),
             columnProperty(Continent.MAX_LIFE_EXPECTANCY, "Max. life expectancy")
                     .columnExpression("max(lifeexpectancy)")
-                    .aggregateColumn(),
+                    .aggregateColumn(true),
             columnProperty(Continent.MIN_INDEPENDENCE_YEAR, "Min. ind. year")
                     .columnExpression("min(indepyear)")
-                    .aggregateColumn(),
+                    .aggregateColumn(true),
             columnProperty(Continent.MAX_INDEPENDENCE_YEAR, "Max. ind. year")
                     .columnExpression("max(indepyear)")
-                    .aggregateColumn(),
+                    .aggregateColumn(true),
             columnProperty(Continent.GNP, "GNP")
                     .columnExpression("sum(gnp)")
-                    .aggregateColumn()
+                    .aggregateColumn(true)
                     .numberFormatGrouping(true))
             .readOnly()
             .caption("Continent");
