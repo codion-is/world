522e977fbd1b8c63a890b5ed04a84638b75c2a8e
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-07-04 12:46:09 +0000

    GPLv3 applied

--- world-client/src/main/java/is/codion/framework/demos/world/model/CountryLanguageTableModel.java
+++ world-client/src/main/java/is/codion/framework/demos/world/model/CountryLanguageTableModel.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Codion World Demo.
+ *
+ * Codion World Demo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Codion World Demo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codion World Demo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2023, Björn Darri Sigurðsson.
+ */
 package is.codion.framework.demos.world.model;
 
 import is.codion.framework.db.EntityConnectionProvider;
