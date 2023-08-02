ddc8d4981f92924f161d5881fb5268fdf45ed243
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2023-08-02 17:33:24 +0000

    CountryReportDataSource improved

--- world-client/src/main/java/is/codion/framework/demos/world/model/CountryReportDataSource.java
+++ world-client/src/main/java/is/codion/framework/demos/world/model/CountryReportDataSource.java
@@ -29,6 +29,7 @@ import is.codion.swing.common.model.worker.ProgressWorker.ProgressReporter;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRField;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
@@ -37,27 +38,29 @@ import java.util.function.Consumer;
 import static is.codion.framework.db.condition.Condition.where;
 import static is.codion.framework.domain.entity.OrderBy.descending;
 
-public final class CountryReportDataSource extends JasperReportsDataSource<Entity> {
+public final class CountryReportDataSource extends JasperReportsDataSource<Country> {
 
   private final EntityConnection connection;
 
   CountryReportDataSource(List<Entity> countries, EntityConnection connection,
                           ProgressReporter<String> progressReporter) {
-    super(countries.iterator(), new CountryValueProvider(),
+    super(Entity.castTo(Country.class, countries).iterator(), new CountryValueProvider(),
             new CountryReportProgressReporter(progressReporter, countries.size()));
     this.connection = connection;
   }
 
   /* See usage in src/main/reports/country_report.jrxml, subreport element */
   public JRDataSource cityDataSource() {
+    Country country = currentItem();
     try {
-      List<Entity> largestCities = connection.select(where(City.COUNTRY_FK)
-              .equalTo(currentItem())
-              .selectBuilder()
-              .selectAttributes(City.NAME, City.POPULATION)
-              .orderBy(descending(City.POPULATION))
-              .limit(5)
-              .build());
+      Collection<City> largestCities = Entity.castTo(City.class,
+              connection.select(where(City.COUNTRY_FK)
+                      .equalTo(country)
+                      .selectBuilder()
+                      .selectAttributes(City.NAME, City.POPULATION)
+                      .orderBy(descending(City.POPULATION))
+                      .limit(5)
+                      .build()));
 
       return new JasperReportsDataSource<>(largestCities.iterator(), new CityValueProvider());
     }
@@ -66,7 +69,7 @@ public final class CountryReportDataSource extends JasperReportsDataSource<Entit
     }
   }
 
-  private static final class CountryValueProvider implements BiFunction<Entity, JRField, Object> {
+  private static final class CountryValueProvider implements BiFunction<Country, JRField, Object> {
 
     private static final String NAME = "name";
     private static final String CONTINENT = "continent";
@@ -75,8 +78,7 @@ public final class CountryReportDataSource extends JasperReportsDataSource<Entit
     private static final String POPULATION = "population";
 
     @Override
-    public Object apply(Entity entity, JRField field) {
-      Country country = entity.castTo(Country.class);
+    public Object apply(Country country, JRField field) {
       return switch (field.getName()) {
         case NAME -> country.name();
         case CONTINENT -> country.continent();
@@ -88,14 +90,13 @@ public final class CountryReportDataSource extends JasperReportsDataSource<Entit
     }
   }
 
-  private static final class CityValueProvider implements BiFunction<Entity, JRField, Object> {
+  private static final class CityValueProvider implements BiFunction<City, JRField, Object> {
 
     private static final String NAME = "name";
     private static final String POPULATION = "population";
 
     @Override
-    public Object apply(Entity entity, JRField field) {
-      City city = entity.castTo(City.class);
+    public Object apply(City city, JRField field) {
       return switch (field.getName()) {
         case NAME -> city.name();
         case POPULATION -> city.population();
@@ -104,7 +105,7 @@ public final class CountryReportDataSource extends JasperReportsDataSource<Entit
     }
   }
 
-  private static final class CountryReportProgressReporter implements Consumer<Entity> {
+  private static final class CountryReportProgressReporter implements Consumer<Country> {
 
     private final AtomicInteger counter = new AtomicInteger();
     private final ProgressReporter<String> progressReporter;
@@ -117,8 +118,8 @@ public final class CountryReportDataSource extends JasperReportsDataSource<Entit
     }
 
     @Override
-    public void accept(Entity country) {
-      progressReporter.publish(country.get(Country.NAME));
+    public void accept(Country country) {
+      progressReporter.publish(country.name());
       progressReporter.setProgress(100 * counter.incrementAndGet() / noOfCountries);
     }
   }
