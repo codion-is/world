4fd2a330e47d0bc7ad9cc9176ec870f2b79a71e8
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-09-10 14:02:51 +0000

    Copyright year updated

--- world-domain/src/test/java/is/codion/framework/demos/world/domain/WorldImplTest.java
+++ world-domain/src/test/java/is/codion/framework/demos/world/domain/WorldImplTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Codion World Demo.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (c) 2004 - 2022, Björn Darri Sigurðsson.
+ * Copyright (c) 2004 - 2024, Björn Darri Sigurðsson.
  */
 package is.codion.framework.demos.world.domain;
 
