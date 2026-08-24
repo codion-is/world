1616ca1338ce71c20e4254d80ec5aa05da0746a4
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2026-08-24 13:12:41 +0000

    Codion upgraded

--- world-client/src/main/java/is/codion/demos/world/ui/WorldAppPanel.java
+++ world-client/src/main/java/is/codion/demos/world/ui/WorldAppPanel.java
@@ -30,7 +30,7 @@ import is.codion.demos.world.model.WorldAppModel;
 import is.codion.plugin.flatlaf.intellij.FlatLookAndFeelIntelliJThemes;
 import is.codion.plugin.flatlaf.intellij.themes.monokaipro.MonokaiPro;
 import is.codion.plugin.flatlaf.themes.FlatLookAndFeelThemes;
-import is.codion.swing.common.ui.component.indicator.ValidIndicator;
+import is.codion.swing.common.ui.component.indicator.ValidationIndicator;
 import is.codion.swing.common.ui.component.table.FilterTableCellRenderer;
 import is.codion.swing.framework.model.SwingEntityModel;
 import is.codion.swing.framework.ui.EntityApplication;
@@ -79,7 +79,7 @@ public final class WorldAppPanel extends EntityApplicationPanel<WorldAppModel> {
 		Locale.setDefault(Locale.of("en", "EN"));
 		EntityPanel.Config.TOOLBAR_CONTROLS.set(true);
 		FilterTableCellRenderer.NUMERICAL_HORIZONTAL_ALIGNMENT.set(SwingConstants.CENTER);
-		ValidIndicator.INDICATOR_CLASS.set("is.codion.plugin.flatlaf.indicator.FlatLafValidIndicator");
+		ValidationIndicator.INDICATOR_CLASS.set("is.codion.plugin.flatlaf.indicator.FlatLafValidationIndicator");
 		ReferentialIntegrityErrorHandling.REFERENTIAL_INTEGRITY_ERROR_HANDLING
 						.set(ReferentialIntegrityErrorHandling.DISPLAY_DEPENDENCIES);
 		FlatLookAndFeelThemes.addAll();
